a7b48aab49b69a397249deb010224ef1ada874bb
Elias <projetosdoelias@gmail.com>
2022-05-01 16:56:42 -0300

    CRUD BASICO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { User } from './user.model';
 import { UserService } from './user.service';
 
@@ -13,5 +13,25 @@ export class UserController {
     @Post('create')
     create(@Body() user: User) {
         return this.service.create(user)
+    }
+
+    @Get('')
+    findAll() {
+        return this.service.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+      return this.service.findOne(id);
+    }
+  
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() user: User) {
+      
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.service.remove(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,4 +15,24 @@ export class UserService {
         const result = await new this.userModel(doc).save();
         return result.id;
     }
+
+    async findAll() {
+        return await this.userModel.find().exec();
+    }
+
+    async findOne(id:string) {
+        return await this.userModel.findById(id).exec();
+    }
+
+    async update(id:string, doc:User) {
+        return await this.userModel.findByIdAndUpdate(id, doc);
+    }
+
+    async remove(id:string) {
+        return await this.userModel.findByIdAndDelete(id)
+    }
+    
+
+
+
 }
